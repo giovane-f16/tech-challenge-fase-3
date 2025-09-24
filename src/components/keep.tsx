a82fa04c2d760f96c2e396eddaa66b1644f559c5
giovanef16 <giovane_05silva@hotmail.com>
2025-09-24 16:07:12 -0300

    Corrigindo inatividade

---
+++ src/components/keep.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import Config from "@/config/config";
+
+const KeepAlive = () => {
+    useEffect(() => {
+        const pingServer = async () => {
+            try {
+                const response = await fetch(`${Config.getApiUrl()}`);
+                if (response.ok) {
+                    console.log("Ping enviado com sucesso");
+                } else {
+                    console.error("Erro no ping", response.status);
+                }
+            } catch (error) {
+                console.error("Erro ao enviar ping", error);
+            }
+        };
+
+        pingServer();
+
+        const intervalId = setInterval(pingServer, 50000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return null;
+};
+
+export default KeepAlive;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import "@/styles/globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import SessionProviderWrapper from "@/providers/session";
+import KeepAlive from "@/components/keep";
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
         <SessionProviderWrapper>
             <div className="flex flex-col min-h-screen">
                 <Header />
+                <KeepAlive />
                 <main className="flex-grow">
                     <Component {...pageProps} />
                 </main>
